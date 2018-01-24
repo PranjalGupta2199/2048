63ac726874528e8685cb05029532fc318c79050f
PranjalGupta2199 <pranjalgupta2199@gmail.com>
2018-01-25 02:20:20 +0530

    Generalised class for Moves

--- try.py
+++ try.py
@@ -1,38 +1,57 @@
-def add (board):
-	for col in range (1,4):
-		for row in range (1,4):
-			if row-1 >= 0 and board[row-1][col] == board[row][col]:
-				board[row-1][col] *= 2
-				board[row][col] = 0
-				break
-
-	return board
-
-def move (board):
-
-	n = 4
-	for col in range (0,n):
-		for row in range (0,n):
-			if row-1 >= 0 and board[row-1][col] == 0 and board[row][col]  != 0:
-				board[row][col], board[row-1][col] = board[row-1][col], board[row][col]
-				board = move(board)
-			else:
-				pass
-	return board
-
-
-
-
 def Print (board):
 	for i in range (0,4):
 		for j in range (0,4):
 			print board[i][j],
 		print
+	print
+	print
 
-board = [[0,0,0,2],[2,0,2,2],[0,4,32,4],[0,4,8,32]]
+class Moves():
+	def __init__(self, direction):
+		if direction == "up": self.n1, self.n2 = 0, -1
+		elif direction == "down": self.n1, self.n2 = 0, 1
+		elif direction == "left": self.n1, self.n2 = -1, 0
+		elif direction == "right": self.n1, self.n2 = 1, 0
+
+	def add (self, board):
+		for col in range (0,4): #n1 = 0 n2 = -1
+			for row in range (0,4):
+				try:
+					if row + self.n2 >= 0 and col + self.n1 >= 0 \
+					and board[row + self.n2][col + self.n1] == board[row][col]:
+						board[row + self.n2][col + self.n1] *= 2
+						board[row][col] = 0
+						continue
+
+				except:
+					pass
+
+		return board
+
+	def move (self, board):
+		for col in range (0,4): #n1 = 0 n2 = -1
+			for row in range (0,4):
+				try:
+					if row + self.n2 >= 0 and col + self.n1 >= 0 \
+					and board[row + self.n2][col + self.n1] == 0 and board[row][col]  != 0:
+						board[row][col], board[row + self.n2][col + self.n1] = board[row + self.n2][col + self.n1], board[row][col]
+						board = self.move(board)
+					else:
+						pass
+				except:
+					pass
+		return board
+
+	def function(self, board):
+		#while self.move(board) != self.add(board):
+		#board = self.move(board)
+		board = self.add(board)
+		board = self.move(board)
+		return board
+
+
+board = [[8,4,0,2],[8,8,4,0],[0,0,0,0],[0,0,0,0]]
 Print(board)
-board = add(board)
-board = move(board)
-print
-print
+RIGHT = Moves('right')
+board = RIGHT.function(board)
 Print(board)
