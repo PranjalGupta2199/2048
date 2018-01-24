1c66ba1c710ae293aeff215915bc5fb361922a37
PranjalGupta2199 <pranjalgupta2199@gmail.com>
2018-01-25 02:50:26 +0530

    Reanamed try.py

---
+++ Key_Function.py
@@ -0,0 +1,69 @@
+import random
+def Print (board):
+	for i in range (0,4):
+		for j in range (0,4):
+			print board[i][j],
+		print
+	print
+	print
+
+class Moves():
+	def __init__(self, direction):
+		if direction == "up": self.n1, self.n2 = 0, -1
+		elif direction == "down": self.n1, self.n2 = 0, 1
+		elif direction == "left": self.n1, self.n2 = -1, 0
+		elif direction == "right": self.n1, self.n2 = 1, 0
+
+	def add (self, board):
+		for col in range (0,4): #n1 = 0 n2 = -1
+			for row in range (0,4):
+				try:
+					if row + self.n2 >= 0 and col + self.n1 >= 0 \
+					and board[row + self.n2][col + self.n1] == board[row][col]:
+						board[row + self.n2][col + self.n1] *= 2
+						board[row][col] = 0
+						continue
+
+				except:
+					pass
+
+		return board
+
+	def move (self, board):
+		for col in range (0,4): #n1 = 0 n2 = -1
+			for row in range (0,4):
+				try:
+					if row + self.n2 >= 0 and col + self.n1 >= 0 \
+					and board[row + self.n2][col + self.n1] == 0 and board[row][col]  != 0:
+						board[row][col], board[row + self.n2][col + self.n1] = board[row + self.n2][col + self.n1], board[row][col]
+						board = self.move(board)
+					else:
+						pass
+				except:
+					pass
+		return board
+
+	def function(self, board):
+		#while self.move(board) != self.add(board):
+		#board = self.move(board)
+		board = self.add(board)
+		board = self.move(board)
+		board = self.append(board)
+		return board
+
+	def append(self, board):
+		block = random.choice([2,4])
+		i,j = random.randint(0,4), random.randint(0,4)
+		while board[i][j] != 0:
+			i,j = random.randint(0,4), random.randint(0,4)
+		board[i][j] = block
+		return board
+
+
+
+
+board = [[8,4,0,2],[8,8,4,0],[0,0,0,0],[0,0,0,0]]
+Print(board)
+RIGHT = Moves('right')
+board = RIGHT.function(board)
+Print(board)
