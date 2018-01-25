a823f518d55754cf3c36b15a5cb851ca17f34d20
PranjalGupta2199 <pranjalgupta2199@gmail.com>
2018-01-25 12:12:27 +0530

    Updated Moves class : For changed to while in add function

--- Key_Function.py
+++ Key_Function.py
@@ -16,16 +16,19 @@ class Moves():
 
 	def add (self, board):
 		for col in range (0,4): #n1 = 0 n2 = -1
-			for row in range (0,4):
+			row = 0
+			while row < 4:
 				try:
 					if row + self.n2 >= 0 and col + self.n1 >= 0 \
 					and board[row + self.n2][col + self.n1] == board[row][col]:
 						board[row + self.n2][col + self.n1] *= 2
 						board[row][col] = 0
+						row +=2
 						continue
 
 				except:
 					pass
+				row += 1
 
 		return board
 
@@ -53,17 +56,20 @@ class Moves():
 
 	def append(self, board):
 		block = random.choice([2,4])
-		i,j = random.randint(0,4), random.randint(0,4)
+		i,j = random.randint(0,3), random.randint(0,3)
+		print i, j, board[i][j]
 		while board[i][j] != 0:
-			i,j = random.randint(0,4), random.randint(0,4)
+			i,j = random.randint(0,3), random.randint(0,3)
+			print i, j, board[i][j]
+			print
 		board[i][j] = block
 		return board
 
 
 
 
-board = [[8,4,0,2],[8,8,4,0],[0,0,0,0],[0,0,0,0]]
+board = [[0,0,0,2],[0,0,0,2],[0,0,0,4],[0,0,0,4]]
 Print(board)
-RIGHT = Moves('right')
-board = RIGHT.function(board)
+down = Moves('down')
+board = down.function(board)
 Print(board)
