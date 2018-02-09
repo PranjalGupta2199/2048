430133f1a2fe05f94611e626ea5b62fdeb0e4010
PranjalGupta2199 <pranjalgupta2199@gmail.com>
2018-02-09 17:30:11 +0530

    Working Components

--- GUI.py
+++ GUI.py
@@ -27,22 +27,29 @@ color can be changed + similar to image load in pygame """
 class MyWindow(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title = "2048 Game")
-
+        Gtk.Window.__init__(self, title = "2048 Game") # use super as it is used in python3
+        self.set_size_request(400,400)
+        self.connect('delete-event', Gtk.main_quit)
 
     def display_button (self,board):
         grid = Gtk.Grid()
         self.add(grid)
+        button = Gtk.Button(label = 'NEW GAME') # use button.set_label when developing UI in code
+        grid.attach(button, 2, 0, 2, 1)
         for row in range (0,len(board)):
             for col in range (0,len(board)):
                     button = Gtk.Button(label = str(board[row][col]))
-                    grid.attach(button, col, row, 1, 1)
-        grid.set_column_homogeneous(True)
-        grid.set_row_homogeneous(True)
+                    grid.attach(button, col, row + 1, 1, 1) # use relative positions
 
-board = [[0,0,0,0],[0,0,0,0],[0,16,16,16],[16,16,16,16]]
-win = MyWindow()
-win.display_button(board)
-win.connect('delete-event', Gtk.main_quit)
-win.show_all()
-Gtk.main()
+        grid.set_column_homogeneous(True)
+        grid.set_column_spacing(10)
+        grid.set_row_homogeneous(True)
+        grid.set_row_spacing(10)
+
+    def on_key_press_event(self, widget, event):
+        pass
+
+    def update_label_text(self):
+        pass
+
+board = [[0 for i in range (4)] for j in range (4)]
--- Key_Function.py
+++ Key_Function.py
@@ -8,14 +8,16 @@ def Print (board):
 	print
 
 class Moves():
+
 	def __init__(self, direction):
-		row,col = 0,0
+		row, col = 0, 0
 		if direction == "up": self.var1 , self.var2,self.n1, self.n2 = col, row, 0, -1
 		elif direction == "down": self.var1, self.var2 ,self.n1, self.n2 = col, row, 0, 1
 		elif direction == "left": self.var1, self.var2 ,self.n1, self.n2 = row, col, -1, 0
 		elif direction == "right": self.var1, self.var2, self.n1, self.n2 = row, col, 1, 0
 
 	def add (self, board):
+		# define logical variable names
 		for self.var1 in range (0,4): #n1 = 0 n2 = -1
 			for self.var2 in range(0,4):
 				try:
@@ -50,23 +52,14 @@ class Moves():
 			while temp1 != temp2:
 				temp1, temp2 = temp2, self.add(self.move(temp2))
 		else:
-			return self.move(self.add(board))
-		return temp1
+			return self.append(self.move(self.add(board)))
+		return self.append(temp1)
 
 
 	def append(self, board):
 		block = random.choice([2,4])
-		i,j = random.randint(0,3), random.randint(0,3)
+		i, j = random.randint(0,3), random.randint(0,3)
 		while board[i][j] != 0:
-			i,j = random.randint(0,3), random.randint(0,3)
+			i, j = random.randint(0,3), random.randint(0,3)
 		board[i][j] = block
 		return board
-
-
-
-
-board = [[2,4,4,2],[2,0,0,2],[2,0,0,2],[2,0,0,2]]
-Print(board)
-down = Moves('left')
-board = down.function(board)
-Print(board)
